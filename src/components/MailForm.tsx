0b7b11b27416f5bc65a89df459d4c41019be5788
Alexander Isom <alexanderdisom@gmail.com>
2024-07-19 22:22:23 +0100

    feat: start implementing contact form

--- src/components/MailForm.tsx
+++ src/components/MailForm.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./ui/button";
 import {
 	Dialog,
 	DialogContent,
@@ -6,19 +7,40 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "./ui/dialog";
+import {
+	TextField,
+	TextFieldInput,
+	TextFieldLabel,
+	TextFieldTextArea,
+} from "./ui/text-field";
 
 export default function MailFormDialog() {
+	const fn = (form: FormData) => {};
 	return (
 		<Dialog>
 			<DialogTrigger>contact</DialogTrigger>
 			<DialogContent>
 				<DialogHeader>
-					<DialogTitle>Are you sure absolutely sure?</DialogTitle>
-					<DialogDescription>
-						This action cannot be undone. This will permanently delete your
-						account and remove your data from our servers.
-					</DialogDescription>
+					<DialogTitle>Contact me</DialogTitle>
 				</DialogHeader>
+				<form>
+					<TextField class="grid w-full  items-center gap-1.5 ">
+						<TextFieldLabel for="name">Your Name</TextFieldLabel>
+						<TextFieldInput type="text" id="name" />
+					</TextField>
+					<TextField class="grid w-full items-center gap-1.5 ">
+						<TextFieldLabel for="email">Your Email</TextFieldLabel>
+						<TextFieldInput type="email" id="email" />
+					</TextField>
+					<TextField class="grid w-full  items-center gap-1.5 ">
+						<TextFieldLabel for="message">Message</TextFieldLabel>
+						<TextFieldTextArea
+							id="message"
+							placeholder="Type your message here."
+						/>
+					</TextField>
+					<Button>Send</Button>
+				</form>
 			</DialogContent>
 		</Dialog>
 	);
