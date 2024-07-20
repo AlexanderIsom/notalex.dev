75220a3d58c7f8830a609137bf9be67d47abfd95
Alexander Isom <37072462+AlexanderIsom@users.noreply.github.com>
2024-07-20 18:37:00 +0100

    feat: add contact form with nodemailer

--- src/components/MailForm.tsx
+++ src/components/MailForm.tsx
@@ -13,21 +13,70 @@ import {
 	TextFieldLabel,
 	TextFieldTextArea,
 } from "./ui/text-field";
-import { createForm } from "@modular-forms/solid";
+import { SubmitHandler, createForm, zodForm } from "@modular-forms/solid";
+import Mail from "nodemailer/lib/mailer";
 
-const formSchema = z.object({
-	email: z.string().email().min(3, {
-		message: "Email must be at least 3 characters",
-	}),
-	name: z.string().min(5, { message: "name must be at least 5 characters" }),
+import nodemailer from "nodemailer";
+import { action, useAction } from "@solidjs/router";
+
+const ContactSchema = z.object({
+	email: z.string().email(),
+	name: z.string().min(2, { message: "Name must be at least 2 characters" }),
 	message: z
 		.string()
-		.min(5, { message: "message must be at least 5 characters" }),
+		.min(5, { message: "Message must be at least 5 characters" }),
+});
+
+type ContactForm = z.infer<typeof ContactSchema>;
+
+const submitForm = action(async (formData: ContactForm) => {
+	"use server";
+	const { email, name, message } = formData;
+
+	const transport = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.MAILER_EMAIL,
+			pass: process.env.MAILER_PASSWORD,
+		},
+	});
+
+	const mailOptions: Mail.Options = {
+		from: process.env.MAILER_EMAIL,
+		to: process.env.MAILER_TARGET,
+		// cc: email, (uncomment this line if you want to send a copy to the sender)
+		subject: `Message from ${name} (${email})`,
+		text: `This is a message via notalex.dev from ${name} : ${message} `,
+	};
+
+	const sendMailPromise = () =>
+		new Promise<string>((resolve, reject) => {
+			transport.sendMail(mailOptions, function (err) {
+				if (!err) {
+					resolve("Email sent");
+				} else {
+					reject(err.message);
+				}
+			});
+		});
+
+	try {
+		await sendMailPromise();
+	} catch (err) {
+		console.log(err);
+	} finally {
+		console.log("success");
+	}
 });
 
 export default function MailFormDialog() {
-	const [contactForm, { Form, Field, FieldArray }] =
-		createForm<z.infer<typeof formSchema>>();
+	const [contactForm, { Form, Field, FieldArray }] = createForm<ContactForm>({
+		validate: zodForm(ContactSchema),
+	});
+
+	const submitMessage = useAction(submitForm);
+
+	// const handleSubmit: SubmitHandler<ContactForm> = (values, event) => {};
 
 	return (
 		<Dialog>
@@ -36,24 +85,55 @@ export default function MailFormDialog() {
 				<DialogHeader>
 					<DialogTitle>Contact me</DialogTitle>
 				</DialogHeader>
-				<form>
-					<TextField class="grid w-full  items-center gap-1.5 ">
+
+				<Form
+					onSubmit={(data) => {
+						submitMessage(data);
+					}}
+					class="flex flex-col gap-4"
+				>
+					<TextField class="grid w-full items-center gap-1.5 ">
 						<TextFieldLabel for="name">Your Name</TextFieldLabel>
-						<TextFieldInput type="text" id="name" />
+						<Field name="name">
+							{(field, props) => (
+								<>
+									<TextFieldInput {...props} type="text" id="name" />
+									{field.error && <div>{field.error}</div>}
+								</>
+							)}
+						</Field>
 					</TextField>
+
 					<TextField class="grid w-full items-center gap-1.5 ">
 						<TextFieldLabel for="email">Your Email</TextFieldLabel>
-						<TextFieldInput type="email" id="email" />
+						<Field name="email">
+							{(field, props) => (
+								<>
+									<TextFieldInput {...props} type="email" id="email" />
+									{field.error && <div>{field.error}</div>}
+								</>
+							)}
+						</Field>
 					</TextField>
-					<TextField class="grid w-full  items-center gap-1.5 ">
+
+					<TextField class="grid w-full items-center gap-1.5 ">
 						<TextFieldLabel for="message">Message</TextFieldLabel>
-						<TextFieldTextArea
-							id="message"
-							placeholder="Type your message here."
-						/>
+						<Field name="message">
+							{(field, props) => (
+								<>
+									<TextFieldTextArea
+										{...props}
+										class="max-h-96"
+										id="message"
+										placeholder="Type your message here."
+									/>
+									{field.error && <div>{field.error}</div>}
+								</>
+							)}
+						</Field>
 					</TextField>
-					<Button>Send</Button>
-				</form>
+					<Button type="submit">Send</Button>
+				</Form>
 			</DialogContent>
 		</Dialog>
 	);
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import MailFormDialog from "./MailForm";
 
 export default function Nav() {
 	return (
-		<nav class=" top-0 sticky flex h-32 backdrop-filter backdrop-blur-lg">
+		<nav class="top-0 sticky flex h-32 backdrop-filter backdrop-blur-lg">
 			<ul class="container flex items-center p-3 justify-center ">
 				<div class="flex font-semibold items-center gap-4">
 					<a href="/">home</a>
